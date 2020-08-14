4cbc2f735b068709dc0ca841ada6cdad2d70a23e
吴新庭 <wu.xinting@hotmail.com>
2020-08-14 20:34:48 +0800

    live assist

--- examples/live_assist/hello_world.py
+++ examples/live_assist/hello_world.py
@@ -1,3 +1,4 @@
+#! /usr/local/bin/python3
 # Hello world example. Doesn't depend on any third party GUI framework.
 # Tested with CEF Python v57.0+.
 
@@ -5,14 +6,14 @@ from cefpython3 import cefpython as cef
 import platform
 import sys
 
-browser = None
+live_room = "http://live.dz11.com/77595203"
 
 def main():
     check_versions()
     sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
     cef.Initialize()
-    browser = cef.CreateBrowserSync(url="https://www.baidu.com/",
-                          window_title="Hello World!")
+    browser = cef.CreateBrowserSync(url=live_room, window_title="Hello World!")
+    set_client_handles(browser)
     cef.MessageLoop()
     cef.Shutdown()
 
@@ -27,6 +28,20 @@ def check_versions():
            arch=platform.architecture()[0]))
     assert cef.__version__ >= "57.0", "CEF Python v57.0+ required to run this"
 
+
+def set_client_handles(browser):
+    browser.SetClientHandler(EventHandler())
+
+
+class EventHandler(object):
+
+    def OnLoadingStateChange(self, browser, is_loading, **_):
+        if is_loading:
+            print('loading...')
+        else:
+            print('loading completed')
+            line = open('alert.js').read()
+            browser.ExecuteJavascript(line)
 
 if __name__ == '__main__':
     main()
